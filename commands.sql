0af2c0c3fdbce7acf977e12fbf4ac11c4abec3ae
dsdshcym <dsdshcym@gmail.com>
2015-06-25 16:01:22 +0800

    Add some other examples

--- commands.sql
+++ commands.sql
@@ -1,15 +1,27 @@
 explain select *
 from basic;
 
+explain select *
+from small_cate_id_small_cate;
+
 #----------------------------------------------------------
 
 explain select name
 from basic;
 
+explain select group_info
+from discount;
+
 #----------------------------------------------------------
 
 explain select distinct name
 from basic;
+
+explain select distinct map_type
+from map_info;
+
+create index index_map_type on map_info(map_type);
+drop index index_map_type on map_info;
 
 #----------------------------------------------------------
 
@@ -48,6 +60,15 @@ where product_rating > 8.5 and environment_rating > 8.5;
 create index index_product_rating on remark(product_rating);
 create index index_environment_rating on remark(environment_rating);
 
+explain select phone from basic
+where alias = '重庆鸡公煲' and name = '巫山烤全鱼';
+
+create index index_name on basic(name);
+drop index index_name on basic;
+
+create index index_alias on basic(alias);
+drop index index_alias on basic;
+
 #----------------------------------------------------------
 
 explain select shop_id
@@ -71,6 +92,12 @@ where SC.city_id = C.city_id and C.city = '上海';
 create index index_city on city_id_city(city);
 drop index index_city on city_id_city;
 
+explain select B.shop_id, B.name
+from basic as B, shop_id_small_cate_id as SS
+where B.shop_id = SS.shop_id and SS.small_cate_id = 'g101';
+
+select small_cate_id from small_cate_id_small_cate limit 1;
+
 #----------------------------------------------------------
 
 explain select city
@@ -79,6 +106,13 @@ inner join city_id_city_pinyin as CP
 where C.city_id = CP.city_id and CP.city_pinyin = 'shanghai';
 
 create index index_city_pinyin on city_id_city_pinyin(city_pinyin);
+
+explain select B.shop_id, D.photos
+from basic as B
+inner join dazhong as D
+where B.shop_id = D.shop_id;
+
+create index index_photos on dazhong(photos);
 
 #----------------------------------------------------------
 
